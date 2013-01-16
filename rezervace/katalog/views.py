2d45f4249aed01cece6939b9eef399c1c15d96fc
Lubos Pechar <lubos.pechar@prirodou.cz>
2013-01-16 16:51:54 +0100

    v0.34 - filtr prvni pokus

--- rezervace/katalog/views.py
+++ rezervace/katalog/views.py
@@ -4,11 +4,26 @@ from django.shortcuts import render
 
 import json
 
-from katalog.models import Rezervace
+from katalog.models import Rezervace, Okres, Status
 
 def home(request):
-    rezervace = Rezervace.objects.all()
-    return render(request, 'home.html', {'rezervace': rezervace,})
+	okresy = Okres.objects.all()
+	rezervace = Rezervace.objects.all()
+	statusy = Status.objects.all()
+	
+	if request.GET.get('okres'):
+		rezervace = rezervace.filter(okres = request.GET.get('okres'))
+		
+	if request.GET.get('status'):
+		rezervace = rezervace.filter(status = request.GET.get('status'))
+	
+	return render(request, 'home.html', 
+		{
+			'rezervace': rezervace,
+			'okresy': okresy,
+			'statusy': statusy,
+		}
+	)
 
 def profil(request, slug):
 	try:
